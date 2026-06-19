4c20a7a795353ccc3af5a1bcce837ea6d2105549
Kalash0098 <kalashpratapwar655@gmail.com>
2026-06-20 00:27:08 +0530

    Add public IP association to EC2

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,5 +29,6 @@ ami = var.ami_id
 instance_type = var.instance_type
 key_name = var.key_name
 vpc_security_group_ids = [aws_security_group.app_sg.id]
+associate_public_ip_address = true
 tags = { Name = "petclinic-server" }
 }
